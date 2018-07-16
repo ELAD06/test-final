cd55965389d35d512a5678bbd87ad9a2b2aed924
Elifer Aponte <eliferaponte_11@hotmail.com>
2018-07-16 00:03:56 -0300

    agregando lista basica

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './List.css';
 
 export default class List extends  Component {
   render() {
     return (
-      <ul>
-        { this.props.results.map((item, index) => <li key={index}><code> {item.title} </code></li>)}
+      <ul className="list-container">
+        { this.props.results.map((item, index) => <li className='item-container' key={index}><code> {item.title} </code></li>)}
       </ul>
     );
   }
