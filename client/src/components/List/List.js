819728b03b8c3e306c7db8fd3413c0078bd642e1
Elifer Aponte <eliferaponte_11@hotmail.com>
2018-07-22 18:53:36 -0300

    agregando indentacion

--- client/src/components/List/List.js
+++ client/src/components/List/List.js
@@ -5,7 +5,11 @@ export default class List extends  Component {
   render() {
     return (
       <ul className="list-container">
-        { this.props.results.map((item, index) => <li className='item-container' key={index}><code> {item.title} </code></li>)}
+        { this.props.results.map(
+          (item, index) => 
+            <li className='item-container' key={index}><code> {item.title} </code></li>
+          )
+        }
       </ul>
     );
   }
