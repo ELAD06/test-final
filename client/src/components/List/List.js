0c42fc29acc88971a36cff09aec9d4f7efb05f47
Elifer Aponte <eliferaponte_11@hotmail.com>
2018-07-22 19:04:54 -0300

    agregando identado

--- client/src/components/List/List.js
+++ client/src/components/List/List.js
@@ -6,18 +6,19 @@ export default class List extends  Component {
     return (
       <ul className="list-container">
         
-        { this.props.isLoading ? (
-          <div className="loading-wrapper">
-          {/* TODO: // CREAR COMPONENTE DE CARGANDO... {SNIPPER} */}
-            Cargando ...
-          </div>
-        ) :
-        (
-          this.props.results.map (
-            (item, index) => 
-              <li className='item-container' key={index}><code> {item.title} </code></li>
+        { this.props.isLoading ? 
+          (
+            <div className="loading-wrapper">
+            {/* TODO: // CREAR COMPONENTE DE CARGANDO... {SNIPPER} */}
+              Cargando ...
+            </div>
+          ) :
+          (
+            this.props.results.map (
+              (item, index) => 
+                <li className='item-container' key={index}><code> {item.title} </code></li>
+            )
           )
-        )
         }
       </ul>
     );
